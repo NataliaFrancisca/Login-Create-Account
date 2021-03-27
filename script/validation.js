ee2328c655c05a477b9f882f79ea4dcd1cef7c7b
Natalia Francisca <nathifrancisca@gmail.com>
2021-03-26 21:28:19 -0300

    Adicionando mensagem de erro na tela

--- script/validation.js
+++ script/validation.js
@@ -1,29 +1,34 @@
 let name = document.getElementById("inputName");
 let email = document.getElementById("inputEmail");
 let password = document.getElementById("inputPassword");
+let textErro = document.querySelector(".errorText");
 
 function validaCamposCreate(){
 
 	if(name.value == "" || name.value.lenght <= 1){
 		alert("Digite um nome válido!")
+		textErro.innerHTML = "Digite um nome válido!";
 		name.focus();
 		return false;
 	}
 
 	if(!validateEmail(email.value)){
 		alert("Digite um email válido!");
+		textErro.innerHTML = "Digite um e-mail válido!";
 		email.focus();
 		return false;
 	}
 	
 	if(password.value == "" || password.value.length < 8){
 		alert("Digite uma senha válida!");
+		textErro.innerHTML = "Digite uma senha válida!";
 		password.focus();
 		return false;
 	}
 
 	if(name.value && email.value && password.value ){
 		alert("Cadastro realizado com sucesso!");
+		textErro.innerHTML = "Cadatro realizado com sucesso!";
 			name.value = "";
 			password.value = "";
 			email.value = "";
@@ -38,18 +43,21 @@ function validaCamposLogin(){
 
 	if(!validateEmail(email.value)){
 		alert("Digite um email válido!");
+		textErro.innerHTML = "Seu e-mail não está correto!";
 		email.focus();
 		return false;
 	}
 
 	if(password.value == "" || password.value.length < 8){
 		alert("Digite uma senha válida!");
+		textErro.innerHTML = "Sua senha não está correta!";
 		password.focus();
 		return false;
 	}
 
 	if(email.value && password.value ){
 		alert("Cadastro realizado com sucesso!");
+		textErro.innerHTML = "Login realizado com sucesso!";
 			email.value = "";
 			password.value = "";
 			email.focus();
